653a4fec512c6533f77dcc64553f0420c7d4665c
JianWu <jwu@csis.dk>
2017-08-30 23:09:28 +0200

    fucking working on learning django

--- mysite/jwu_site/views.py
+++ mysite/jwu_site/views.py
@@ -2,8 +2,25 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.loader import get_template
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is JianWu\'s personal site')
+    # I am a cow-worker!
+    cow_worker = (
+        ' ___________________         \n'
+        '< I am a cow-worker >        \n'
+        ' -------------------         \n'
+        '       \   ^__^              \n'
+        '        \  (oo)\_______      \n'
+        '           (__)\       )\/\  \n'
+        '               ||----w |     \n'
+        '               ||     ||     \n'
+    )
+
+    return render_to_response(
+        'I-am-co-worker.html',
+        {'cow_worker': cow_worker}
+    )
