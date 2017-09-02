4941a3196d58fbb72a7ba81052785d4e1bca5e74
JaggerWu <xcb479@alumni.ku.dk>
2017-09-02 15:42:22 +0200

    Built about_me.html

--- mysite/jwu_site/views.py
+++ mysite/jwu_site/views.py
@@ -8,23 +8,5 @@ from django.template.loader import get_template
 
 # Create your views here.
 def about_me(request):
-    # I am a cow-worker!
-    cow_worker = (
-        '''
-         ___________________        <br/>
-         < I am a cow-worker >      <br/>
-         -------------------        <br/>
-               \   ^__^             <br/>
-                \  (oo)\_______     <br/>
-                   (__)\       )\/\ <br/>
-                       ||----w |    <br/>
-                       ||     ||    <br/>
-        '''
-        
-    )
-    
-    return render(
-        request, 
-        'I-am-co-worker.html',
-        {'cow_worker': cow_worker}
-    )
+    # There are some introduction for myself.
+    return render(request, 'about_me.html')
