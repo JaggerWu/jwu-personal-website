d9875d0f1727a5f9172fba4db1c8fe3af13404e4
JaggerWu <xcb479@alumni.ku.dk>
2017-11-12 10:49:39 +0100

    Add the settting

--- mysite/jwu_site/urls.py
+++ mysite/jwu_site/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^index', views.index, name='jianwu'),
     url(r'^about_me', views.about_me, name='about_me'),
+    url(r'^download_cv', views.download_cv, name='download_cv'),
 ]
--- mysite/jwu_site/views.py
+++ mysite/jwu_site/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.loader import get_template
+from django.http import StreamingHttpResponse
+import os
 
 
 # Create your views here.
@@ -14,3 +16,19 @@ def index(request):
 def about_me(request):
     # There are some introduction for myself.
     return render(request, 'about_me.html')
+
+local_path = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+
+def download_cv(request):
+    file_name = os.path.join(local_path, 'file/Weed_detection.pdf')
+    with open(file_name) as f:
+        c = f.read()
+
+    response = HttpResponse(c)
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format('JianWu_cv.pdf')
+
+    return response
