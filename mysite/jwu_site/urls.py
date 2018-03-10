dbe2d7ade3d034e1fd903125d47ba5ddc100cd64
Jian Wu <xcb479@alumni.ku.dk>
2018-03-10 23:34:00 +0100

    default inde

--- mysite/jwu_site/urls.py
+++ mysite/jwu_site/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^index', views.index, name='jianwu'),
-    url(r'^about_me', views.about_me, name='about_me'),
-    url(r'^cv', views.cv, name='JianWu_CV'),
-    url(r'^download_cv', views.download_cv, name='download_cv'),
+    path('', views.index, name='jianwu'),
+    path('about_me', views.about_me, name='about_me'),
+    path('cv', views.cv, name='JianWu_CV'),
+    path('download_cv', views.download_cv, name='download_cv'),
 ]
